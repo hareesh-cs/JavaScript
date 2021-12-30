65675e58773ef53ec5c82f2a62dc8f09b431e0cd
Hareesh Sikakollu <hareeshsikakollu@gmail.com>
2021-12-30 10:29:34 +0530

    Assingment 4 and 5

---
+++ Assignment-5.js
@@ -0,0 +1,38 @@
+// Refactor the following function into a one-liner:
+// const printName = (name) => {
+//                      return “Hi” + name;
+//            }
+
+const printName = (name) => "Hi " + name;
+
+
+
+// Rewrite the following code using template literals
+// const printBill = (name, bill) => {
+//                      return “Hi “ + name + “, please pay: “ + bill;
+//            }
+
+const printBill = (name, bill) => `Hi ${name}, please pay: ${bill} `;
+
+
+// Modify the following code such that the object properties are destructured and logged.
+// const person = {
+//                       name: “Noam Chomsky”,
+//                       age: 92
+//             }  
+//            let name = person.name;
+//            let age = person.age;
+//            console.log(name);
+//            console.log(age);
+  
+const person = {
+    name: "Noam Thomsky",
+    age: 92,
+    bill:"50$",
+  };
+  
+  let {name,age,bill } = person;
+ console.log( printName(name));
+ console.log( printBill(name,bill));
+  console.log(name);
+  console.log(age);
